12a0b07aa86f744bb3295ead688398af1d1c3f98
Hiroaki Asakawa <hiroaki.asakawa@dsv.daikin.com>
2025-01-28 15:25:57 -0800

    ZEN-11153 Fix css import to include css in module

--- layout/src/bootstrap.js
+++ layout/src/bootstrap.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router";
-
-import './index.css';
+import { BrowserRouter } from 'react-router';
 
 import Footer from './components/Footer';
 import Header from './components/Header';
--- layout/src/components/Header.js
+++ layout/src/components/Header.js
@@ -1,44 +1,37 @@
 import React from 'react';
-import { NavLink } from 'react-router';
-
-import User from './User';
-import { NavLink, Link } from "react-router";
+import { NavLink, Link } from 'react-router';
 
 import Logo from '../assets/daikin_logo.png';
-
+import '../index.css';
+import User from './User';
 
 const Header = ({ navigationItems = [] }) => {
-
     return (
-            <header
-                className='flex flex-row justify-between items-center font-daikinSerif h-16 w-full min-w-[360px] border-b-1 border-(--dds-color-divider) pl-3 pr-4'
-            >
-                <div 
-                    className='flex items-center gap-4'
-                >
-                    <Link to={navigationItems[0]?.path} className='flex items-center h-16'>
-                        <img
-                            src={Logo}
-                            alt='Daikin'
-                            className='h-8 min-w-[115.52px]'
-                        />
-                    </Link>
-                    <nav>
-                        <ul className='flex'>
-                            {navigationItems.map((item) => (
-                                <NavLink key={item.name} to={item.path} className={({ isActive }) =>
-                                    isActive 
+        <header className="flex flex-row justify-between items-center font-daikinSerif h-16 w-full min-w-[360px] border-b-1 border-(--dds-color-divider) pl-3 pr-4">
+            <div className="flex items-center gap-4">
+                <Link to={navigationItems[0]?.path} className="flex items-center h-16">
+                    <img src={Logo} alt="Daikin" className="h-8 min-w-[115.52px]" />
+                </Link>
+                <nav>
+                    <ul className="flex">
+                        {navigationItems.map((item) => (
+                            <NavLink
+                                key={item.name}
+                                to={item.path}
+                                className={({ isActive }) =>
+                                    isActive
                                         ? 'flex items-center whitespace-nowrap h-16 px-4 pt-1 text-(--dds-color-common-brand-default) hover:text-(--dds-color-common-brand-hover) focus:text-(--dds-color-common-brand-press) font-(--dds-font-weight-bold) border-b-4 border-(--dds-color-common-brand-default) hover:border-(--dds-color-common-brand-hover) focus:border-(--dds-color-common-brand-press)'
                                         : 'flex items-center whitespace-nowrap h-16 px-4 text-(--dds-color-common-neutral-default) hover:text-(--dds-color-common-neutral-hover) focus:text-(--dds-color-common-neutral-press)'
-                                  }>
-                                    {item.name}
-                                </NavLink>
-                            ))}
-                        </ul>
-                    </nav>
-                </div>
-                <User />
-            </header>
+                                }
+                            >
+                                {item.name}
+                            </NavLink>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+            <User />
+        </header>
     );
 };
 
