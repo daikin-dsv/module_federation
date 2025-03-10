87a9e2b5d135c7b3eea0ec5ebd3e807c612d22a3
Niral Bhavsar <niral.bhavsar@dsv.daikin.com>
2025-03-10 11:24:43 -0700

    ZEN-11513 Update Keycloack Client ID

--- layout/src/context/Auth/keycloak.js
+++ layout/src/context/Auth/keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 const SSO_URL = 'https://sso-dev.daikinlab.com/auth';
 const SSO_REALM = 'daikin';
-const SSO_CLIENT = 'rad-test2';
+const SSO_CLIENT = 'rad-test';
 
 const keycloak = new Keycloak({
     url: SSO_URL,
