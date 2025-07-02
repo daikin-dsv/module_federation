88a5080b6bd861db455817568a03dc923ef989d0
Niral Bhavsar <niral.bhavsar@dsv.daikin.com>
2025-07-02 15:33:48 -0700

    Revert "ZEN-12087 Move ModuleFederationPlugin to shared"
    This reverts commit c8b987d7bdead656845c0a21cebf0e7683688abd.

--- regional-app-1/rspack.config.js
+++ regional-app-1/rspack.config.js
@@ -3,7 +3,7 @@ const { HtmlRspackPlugin, rspack, DefinePlugin } = require('@rspack/core');
 const { merge } = require('webpack-merge');
 
 const modeConfig = (env) => require(`./build-utils/rspack.${env}`)(env);
-const shared = require('./build-utils/moduleFederationShared');
+const dependencies = require('./package.json').dependencies;
 const { REGIONAL_APP_1, LAYOUT, WIDGETS } = require('../config');
 
 module.exports = ({ mode }) => {
@@ -48,11 +48,38 @@ module.exports = ({ mode }) => {
             plugins: [
                 new ModuleFederationPlugin({
                     name: REGIONAL_APP_1.NAME,
-                    remotes: {
-                        Layout: `${LAYOUT.NAME}@http://localhost:${LAYOUT.PORT}/remoteEntry.js`,
-                        Widget: `${WIDGETS.NAME}@http://localhost:${WIDGETS.PORT}/remoteEntry.js`,
-                    },
-                    shared
+                    remotes: mode === 'production'
+                        ? {
+                            Layout: `${LAYOUT.NAME}@${process.env.LAYOUT_URL}/remoteEntry.js`,
+                            Widget: `${WIDGETS.NAME}@${process.env.WIDGETS_URL}/remoteEntry.js`
+                        }
+                        : {
+                            Layout: `${LAYOUT.NAME}@http://localhost:${LAYOUT.PORT}/remoteEntry.js`,
+                            Widget: `${WIDGETS.NAME}@http://localhost:${WIDGETS.PORT}/remoteEntry.js`,
+                        },
+                    shared: {
+                        react: {
+                            singleton: true,
+                            requiredVersion: dependencies.react
+                        },
+                        'react-dom': {
+                            singleton: true,
+                            requiredVersion: dependencies['react-dom']
+                        },
+                        'react-router': {
+                            singleton: true,
+                            requiredVersion: dependencies['react-router']
+                        },
+                        '@daikin-oss/design-system-web-components': {
+                            singleton: true,
+                            requiredVersion:
+                                dependencies['@daikin-oss/design-system-web-components']
+                        },
+                        '@daikin-oss/dds-tokens': {
+                            singleton: true,
+                            requiredVersion: dependencies['@daikin-oss/dds-tokens']
+                        }
+                    }
                 }),
                 new HtmlRspackPlugin({
                     template: './index.html',
