75b0fcd44bd97b34db267cb2882d17a8f5f591f4
Niral Bhavsar <niral.bhavsar@dsv.daikin.com>
2026-02-26 16:09:40 -0800

    BR-887 Fix Docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,10 +14,10 @@ const config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://laughing-doodle-gz3872k.pages.github.io',
+    url: 'https://daikin-dsv.github.io/module_federation/docs/',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/docs/',
+    baseUrl: '/module_federation/docs/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
